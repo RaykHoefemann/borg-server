4eb6a1a67d864a5033e3cab10d3593a182a242f1
Rayk Höfemann <rayk.hoefemann@mail.de>
2026-04-21 20:32:53 +0200

    new script added

---
+++ podman-scripts/01-ssh-set-user-key.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# 01-ssh-set-user-key.sh
+# ----------------------
+# Set the public key for an existing user.
+# required:
+#   - ssh-file
+#   - or ssh key string directly
+#
+# Usage:
+#   ./scripts/01-ssh-set-user-key.sh <username> <keyfile|keystring>
+#   ./scripts/01-ssh-set-user-key.sh test "ssh-ed25519 AAAA…"
+#   ./scripts/01-ssh-set-user-key.sh test test-key.pub
+#
+
+KEYDIR="config/keys"
+CONF="config/clients.conf"
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <username> <public-key-file|public-key-string>"
+    exit 1
+fi
+
+USERNAME="$1"
+INPUT="$2"
+
+# check if user exists
+if ! grep -q "^${USERNAME}:" "$CONF"; then
+    echo "ERROR: user '$USERNAME' does not exists in clients.conf!"
+    exit 1
+fi
+
+TARGET="${KEYDIR}/${USERNAME}.pub"
+mkdir -p "$KEYDIR"
+
+# Fall 1: INPUT is a existing file
+if [ -f "$INPUT" ]; then
+    echo "[key] Read key from file: $INPUT"
+    cp "$INPUT" "$TARGET"
+    OK=1
+else
+    # Fall 2: INPUT is a Key-String
+    echo "$INPUT" | grep -q "^ssh-" && OK=1
+    if [ "$OK" = "1" ]; then
+        echo "[key] Write key string in file: $TARGET"
+        echo "$INPUT" > "$TARGET"
+    else
+        echo "ERROR: '$INPUT' isn't a file or a valid ssh key!"
+        exit 1
+    fi
+fi
+
+echo "[key] Public key saved in: $TARGET"
+echo "→ Please restart the container:"
+
