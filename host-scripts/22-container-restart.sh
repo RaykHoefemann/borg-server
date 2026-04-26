78f7714f8dea1a3a390e69f1145c9d641fa39a5e
Rayk Höfemann <107184853+RaykHoefemann@users.noreply.github.com>
2026-04-26 08:38:50 +0200

    Update 22-container-restart.sh

--- host-scripts/22-container-restart.sh
+++ host-scripts/22-container-restart.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #
-# 23-container-restart.sh
+# 22-container-restart.sh
 # --------------
 # Restarts the Borg server container.
 # Must be executed when clients.conf has been modified.
 #
 # Usage:
-#   ./scripts/23-container-restart.sh
+#   ./scripts/22-container-restart.sh
 #
 # Example:
-#   ./scripts/23-container-restart.sh
+#   ./scripts/22-container-restart.sh
 #
 
 echo "[restart] Restarting Borg server..."
